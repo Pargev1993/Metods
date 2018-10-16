687ac884c69a153218a8544c59b950dc378bab29
pqo1993 <pqoalla@gmail.com>
2018-10-16 16:39:30 +0400

    factorial

---
+++ metod recursia/metod recursia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metod_recursia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int num = int.Parse(Console.ReadLine());
+                int res = fact(num);
+                Console.WriteLine(res);
+                Console.ReadKey();
+            }
+        }
+        static int fact (int num)
+        {
+            if (num == 1) return 1;
+            return num * fact(num - 1);
+            
+        }
+    }
+}
