9e6086ce76f6ae57ba8025359a9c95cc4b7119d3
pqo1993 <pqoalla@gmail.com>
2018-10-17 22:19:05 +0400

    recursia

---
+++ recursia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+           recursia(10);
+          
+            Console.ReadKey();
+        }
+        static int recursia(int n)
+        {
+            if (n <1)
+                return 1;
+            Console.WriteLine(n);
+            return  recursia(n - 1);
+            
+        }
+    }
+}
