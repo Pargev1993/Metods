68af40779dc36c682613c39e8d3632fceb7b1e6a
pqo1993 <pqoalla@gmail.com>
2018-10-19 13:10:49 +0400

    Եռանիշ թվի առաջին թվանշանը ջնջել։ Եթե ստացված թիվը բազմապատկվի 7-ով, կստացվի սկզբնական թիվը։ Գտնել այդպիսի թվերը։

---
+++ home_29/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            threedigitnumber(0);
+            Console.ReadKey();
+        }
+        static void threedigitnumber(int num)
+        {
+            
+            for (int i = 100; i < 1000; i++)
+            {
+
+                if (i == i%100*7)
+
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
