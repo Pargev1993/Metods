ebe48d1917d08fdc253b515d09ff4e7bceb67a59
pqo1993 <pqoalla@gmail.com>
2018-10-19 12:30:20 +0400

    Ներածել n թիվը։ Արտածել yes, եթե այն հավասար է իր թվանշանների գումարի կրկնապատիկին, no՝ հակառակ դեպքում։

---
+++ home_28/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x == valu(x))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+                Console.WriteLine("no");
+            Console.ReadKey();
+        }
+        static int valu(int x)
+        {
+            int result = 0;
+
+            for (int i = 0; i <= x; i++)
+            {
+
+                result += x % 10;
+                x /= 10;
+            }
+            result = 2 * result;
+            return result;
+        }
+    }
+}
